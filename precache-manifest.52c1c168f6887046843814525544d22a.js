df96c33c65722a278c1aa6c308f6092a60237619
Horlarmeday <39765147+Horlarmeday@users.noreply.github.com>
2021-02-20 20:33:44 +0000

    Deploying to gh-pages from  @ f66522db8e0252f2759396899c4f88865be40f2d 🚀

---
+++ precache-manifest.52c1c168f6887046843814525544d22a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d917b21f6d477d974dfd85f6a9b00f0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3ab975559fe275b0c25e",
+    "url": "/portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "35ced101af5f7d7e9437",
+    "url": "/portfolio/static/css/4.19dfc3d0.chunk.css"
+  },
+  {
+    "revision": "5a0264b2499ff9762aee",
+    "url": "/portfolio/static/css/main.1a718bf8.chunk.css"
+  },
+  {
+    "revision": "0da6a26dd7785c2c0623",
+    "url": "/portfolio/static/js/2.3a6459ae.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/portfolio/static/js/2.3a6459ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab975559fe275b0c25e",
+    "url": "/portfolio/static/js/3.7f2d9fad.chunk.js"
+  },
+  {
+    "revision": "35ced101af5f7d7e9437",
+    "url": "/portfolio/static/js/4.3f2ba1f5.chunk.js"
+  },
+  {
+    "revision": "5a0264b2499ff9762aee",
+    "url": "/portfolio/static/js/main.4f391fdb.chunk.js"
+  },
+  {
+    "revision": "d24815f49f29c710fdbb",
+    "url": "/portfolio/static/js/runtime-main.124d98a7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1e3e208420e6386fd4ccac51c203d217",
+    "url": "/portfolio/static/media/centrifugelogo.1e3e2084.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "9c5845abe2afac7d5e5d356ef05393cf",
+    "url": "/portfolio/static/media/futalogo.9c5845ab.png"
+  },
+  {
+    "revision": "0bd7919f3494eaf872a5728b7e819416",
+    "url": "/portfolio/static/media/log.0bd7919f.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "996bb05d60a5bb07303e2601969b2c87",
+    "url": "/portfolio/static/media/mkanlogo.996bb05d.jpg"
+  },
+  {
+    "revision": "c129541f1cd37a2cda2dee7973eeeae7",
+    "url": "/portfolio/static/media/okra.c129541f.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "4bbfe6a1503afd7bb449281fa89e98dc",
+    "url": "/portfolio/static/media/topclasslogo.4bbfe6a1.jpg"
+  }
+]);
